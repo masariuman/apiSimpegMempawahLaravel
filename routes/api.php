88907ad2e4a0f36bcd6c29ede2cdb25596ebde06
masariuman <masariuman@gmail.com>
2024-04-26 07:55:35 +0700

    change route from web to api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix' => 'v2'], function () {
+    Route::group(['prefix' => 'emp'], function () {
+        Route::get('/', 'EMPController@aktif');
+        Route::get('/cpns', 'EMPController@cpns');
+        Route::get('/pns', 'EMPController@pns');
+        Route::get('/pppk', 'EMPController@pppk');
+        Route::get('/pensiun', 'EMPController@pensiun');
+        Route::get('/pindah', 'EMPController@pindah');
+        Route::get('/all', 'EMPController@all');
+        Route::get('/{nip}', 'EMPController@person');
+    });
+    Route::group(['prefix' => 'sipmewah'], function () {
+        Route::get('/dashboard', 'DashboardSimpegController@dashboard');
+        Route::get('/aktif', 'DashboardSimpegController@aktif');
+    });
 });
--- routes/web.php
+++ routes/web.php
@@ -3,24 +3,24 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Controller;
 
-Route::group(['prefix' => 'v2'], function () {
-    Route::group(['prefix' => 'api'], function () {
-        Route::group(['prefix' => 'emp'], function () {
-            Route::get('/', 'EMPController@aktif');
-            Route::get('/cpns', 'EMPController@cpns');
-            Route::get('/pns', 'EMPController@pns');
-            Route::get('/pppk', 'EMPController@pppk');
-            Route::get('/pensiun', 'EMPController@pensiun');
-            Route::get('/pindah', 'EMPController@pindah');
-            Route::get('/all', 'EMPController@all');
-            Route::get('/{nip}', 'EMPController@person');
-        });
-        Route::group(['prefix' => 'sipmewah'], function () {
-            Route::get('/dashboard', 'DashboardSimpegController@dashboard');
-            Route::get('/aktif', 'DashboardSimpegController@aktif');
-        });
-    });
-});
+// Route::group(['prefix' => 'v2'], function () {
+//     Route::group(['prefix' => 'api'], function () {
+//         Route::group(['prefix' => 'emp'], function () {
+//             Route::get('/', 'EMPController@aktif');
+//             Route::get('/cpns', 'EMPController@cpns');
+//             Route::get('/pns', 'EMPController@pns');
+//             Route::get('/pppk', 'EMPController@pppk');
+//             Route::get('/pensiun', 'EMPController@pensiun');
+//             Route::get('/pindah', 'EMPController@pindah');
+//             Route::get('/all', 'EMPController@all');
+//             Route::get('/{nip}', 'EMPController@person');
+//         });
+//         Route::group(['prefix' => 'sipmewah'], function () {
+//             Route::get('/dashboard', 'DashboardSimpegController@dashboard');
+//             Route::get('/aktif', 'DashboardSimpegController@aktif');
+//         });
+//     });
+// });
 
 // Route::get('/', function () {
 //     return view('welcome');
